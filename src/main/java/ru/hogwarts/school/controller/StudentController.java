6672e1fe96282c8e750daefa45fd589de0a23a5f
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-03-08 21:22:45 +0300

    addAvatarVers1

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,9 +1,11 @@
 package ru.hogwarts.school.controller;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
+
 
 import java.util.Collection;
 
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -15,6 +15,10 @@ public class Student {
     @JoinColumn(name ="faculty_id")
     private Faculty faculty;
 
+    public Student() {
+
+    }
+
 
     public int getId() {
         return id;
@@ -40,25 +44,7 @@ public class Student {
         this.age = age;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id && age == student.age && Objects.equals(name, student.name);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+
 }
