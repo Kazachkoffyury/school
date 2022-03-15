2ca9dd0299e5874af0711643b06ce275683012a2
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-03-15 16:48:31 +0300

    addTestIntegrations

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,50 @@
 package ru.hogwarts.school;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Student;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     void contextLoads() {
     }
+
+
+
+    @Test
+    public void testGetStudent() throws  Exception{
+        Assertions
+                .assertThat(this.testRestTemplate.getForObject("http://localhost:"+port+"/student",String.class)).isNotNull();
+    }
+
+    @Test
+    public void testPostStudent() throws Exception {
+        Student student = new Student();
+        student.setId(1);
+        student.setAge(45);
+        student.setName("vasya");
+
+        Assertions
+                .assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, String.class))
+                .isNotNull();
+    }
+
+
 
 }
